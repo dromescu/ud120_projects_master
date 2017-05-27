7576b71ba0a1d8279dcada8940dbb50e95a96ae4
dromescu <dromescu@gmail.com>
2017-05-27 12:03:01 +0200

    Modified mack to 10% email preprocess

--- tools/email_preprocess.py
+++ tools/email_preprocess.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python
 
 import pickle
-import cPickle
 import numpy
 
-from sklearn import cross_validation
+from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_selection import SelectPercentile, f_classif
 
@@ -18,28 +17,19 @@ def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/ema
             -- splits into training/testing sets (10% testing)
             -- vectorizes into tfidf matrix
             -- selects/keeps most helpful features
-
         after this, the feaures and labels are put into numpy arrays, which play nice with sklearn functions
-
         4 objects are returned:
             -- training/testing features
             -- training/testing labels
-
     """
 
     ### the words (features) and authors (labels), already largely preprocessed
     ### this preprocessing will be repeated in the text learning mini-project
-    authors_file_handler = open(authors_file, "r")
-    authors = pickle.load(authors_file_handler)
-    authors_file_handler.close()
+    word_data = pickle.load( open(words_file, "rb")) #read files as bytes to work on p3
+    authors = pickle.load( open(authors_file, "rb") )
 
-    words_file_handler = open(words_file, "r")
-    word_data = cPickle.load(words_file_handler)
-    words_file_handler.close()
-
-    ### test_size is the percentage of events assigned to the test set
-    ### (remainder go into training)
-    features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
+    ### test_size is the percentage of events assigned to the test set (remainder go into training)
+    features_train, features_test, labels_train, labels_test = train_test_split(word_data, authors, test_size=0.1, random_state=42)
 
 
 
@@ -53,13 +43,17 @@ def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/ema
 
     ### feature selection, because text is super high dimensional and
     ### can be really computationally chewy as a result
-    selector = SelectPercentile(f_classif, percentile=10)
+
+    #Changed for desission tree.
+    #selector = SelectPercentile(f_classif, percentile=1)
+    selector = SelectPercentile(f_classif, percentile=1)
     selector.fit(features_train_transformed, labels_train)
     features_train_transformed = selector.transform(features_train_transformed).toarray()
     features_test_transformed  = selector.transform(features_test_transformed).toarray()
 
     ### info on the data
-    print "no. of Chris training emails:", sum(labels_train)
-    print "no. of Sara training emails:", len(labels_train)-sum(labels_train)
+    print ("no. of Chris training emails:", sum(labels_train))
+    print ("no. of Sara training emails:", len(labels_train)-sum(labels_train))
+
 
     return features_train_transformed, features_test_transformed, labels_train, labels_test
