3231aec57f58f9034bb3160765aca68093a07a3f
dromescu <dromescu@gmail.com>
2017-05-28 15:13:46 +0200

    outliers mini project

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -1,3 +1,4 @@
+### your code below
 #!/usr/bin/python
 
 import pickle
@@ -8,12 +9,17 @@ from feature_format import featureFormat, targetFeatureSplit
 
 
 ### read in data dictionary, convert to numpy array
-data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "rb") )
+data_dict.pop( "TOTAL", 0 )
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
-
 ### your code below
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
 
-
-
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+import operator
 
 def outlierCleaner(predictions, ages, net_worths):
     """
@@ -7,14 +7,17 @@ def outlierCleaner(predictions, ages, net_worths):
         residual errors (difference between the prediction
         and the actual net worth).
 
-        Return a list of tuples named cleaned_data where 
+        Return a list of tuples named cleaned_data where
         each tuple is of the form (age, net_worth, error).
     """
-    
+
     cleaned_data = []
 
     ### your code goes here
+    errors = [a-b for a,b in zip(predictions, net_worths)]
+    data = list(zip(ages, net_worths, errors))
+    data.sort(key=operator.itemgetter(2))
+    cleaned_data = data[:int(len(predictions)*0.9)]
 
-    
+
     return cleaned_data
-
