97eaefffd9821f1a90221e5c80767be0b972eeef
dromescu <dromescu@gmail.com>
2017-05-28 23:15:38 +0200

    Finished Feature Selection

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python
 
 import pickle
-import numpy
-numpy.random.seed(42)
+import numpy as np
 
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+np.random.seed(42)
 
 ### The words (features) and authors (labels), already largely processed.
 ### These files should have been created from the previous (Lesson 10)
 ### mini-project.
-words_file = "../text_learning/your_word_data.pkl" 
+words_file = "../text_learning/your_word_data.pkl"
 authors_file = "../text_learning/your_email_authors.pkl"
-word_data = pickle.load( open(words_file, "r"))
-authors = pickle.load( open(authors_file, "r") )
+word_data = pickle.load( open(words_file, "rb"))
+authors = pickle.load( open(authors_file, "rb") )
 
 
 
@@ -19,8 +23,8 @@ authors = pickle.load( open(authors_file, "r") )
 ### remainder go into training)
 ### feature matrices changed to dense representations for compatibility with
 ### classifier functions in versions 0.15.2 and earlier
-from sklearn import cross_validation
-features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
+
+features_train, features_test, labels_train, labels_test = train_test_split(word_data, authors, test_size=0.1, random_state=42)
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
@@ -38,6 +42,30 @@ labels_train   = labels_train[:150]
 
 
 ### your code goes here
+clf = DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print ("Accuracy:", accuracy_score(labels_test, pred))
+
+print("Important features:")
+for index, feature in enumerate(clf.feature_importances_):
+    if feature>0.2:
+        print("feature no", index)
+        print("importance", feature)
+        print("word", vectorizer.get_feature_names()[index])
 
 
-
+# clf = DecisionTreeClassifier(min_samples_split=40)
+#
+# clf.fit(features_train, labels_train)
+# pred = clf.predict(features_test)
+#
+# acc = accuracy_score(labels_test, pred)
+# print("Accuracy: ", acc)
+#
+# print(clf.feature_importances_.argmax())
+# print(vectorizer.get_feature_names()[clf.feature_importances_.argmax()])
+#
+# for counter, importance in enumerate(clf.feature_importances_):
+#     if importance > 0.2:
+#         print("Counter and importance ", counter, " - ", importance, " ", vectorizer.get_feature_names()[counter])
